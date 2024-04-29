ebdb17678733049ffe06168d40f38293fbeb0d51
James Rhodes <james21198@gmail.com>
2024-04-29 21:53:48 +0100

    API Data Updated

--- public/js/api.js
+++ public/js/api.js
@@ -18,13 +18,27 @@ const createSearchBox = (bookName, bookItem, index) => {
     }
 }
 
+const generateResults = (data) => {
+    data.forEach((bookItem, index) => {
+        const bookTitle = bookItem.title;
+        console.log(bookTitle)
+        const bookAuthor = bookItem.author_name[0];
+        console.log(bookAuthor)
+        const bookPublished = bookItem.first_publish_year;
+        console.log(bookPublished)
+        const bookIsbn = bookItem.isbn[0];
+        console.log(bookIsbn)
+        const bookCoverImage = bookItem.cover_i;
+        console.log(bookCoverImage)
+    });
+}
 
 const getSearchDetails = (bookName) => {
     var apiUrl = `${BOOK_API_BASE_URL}search.json?q=${bookName}`
     fetch(apiUrl)
         .then(res => res.json())
         .then(data => 
-        console.log(data)
+        generateResults(data.docs)
     )
     .catch (() => {
         alert("An error has occurred whilst finding this search request!");
